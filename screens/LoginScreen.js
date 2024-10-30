667599f43fc01582e3fcc85d0cb70c7ad8717c9e
Marcos Filipe de Paula Macêdo <marcosfilipe.macedo@gmail.com>
2024-10-29 21:12:29 -0300

    changing style

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,4 +1,3 @@
-// screens/LoginScreen.js
 import React, { useState } from 'react';
 import { View, TextInput, StyleSheet, Text, Image, Alert, TouchableOpacity } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
@@ -25,35 +24,40 @@ export default function LoginScreen({ navigation }) {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.welcomeText}>Bem-vindo ao...</Text>
-      <Image source={require('../assets/login-image.png')} style={styles.image} />
-      <View style={styles.inputContainer}>
-        <TextInput
-          style={styles.input}
-          placeholder="Usuário"
-          value={username}
-          onChangeText={setUsername}
-        />
-        <Icon name="mail-outline" size={24} color="#888" style={styles.icon} />
-      </View>
-      <View style={styles.inputContainer}>
-        <TextInput
-          style={styles.input}
-          placeholder="Senha"
-          secureTextEntry={!showPassword}
-          value={password}
-          onChangeText={setPassword}
-        />
-        <TouchableOpacity onPress={() => setShowPassword(!showPassword)} style={styles.icon}>
-          <Icon name={showPassword ? "eye-outline" : "eye-off-outline"} size={24} color="#888" />
+      <Text style={styles.welcomeText}>LOGIN</Text>
+      <Image source={require('../assets/logo.png')} style={styles.image} />
+
+      <View style={styles.card}>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            placeholder="Usuário"
+            placeholderTextColor="#8F8F8F"
+            value={username}
+            onChangeText={setUsername}
+          />
+          <Icon name="mail-outline" size={24} color="#000" style={styles.icon} />
+        </View>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            placeholder="Senha"
+            placeholderTextColor="#8F8F8F"
+            secureTextEntry={!showPassword}
+            value={password}
+            onChangeText={setPassword}
+          />
+          <TouchableOpacity onPress={() => setShowPassword(!showPassword)} style={styles.icon}>
+            <Icon name={showPassword ? "eye-outline" : "eye-off-outline"} size={24} color="#fff" />
+          </TouchableOpacity>
+        </View>
+        <TouchableOpacity style={styles.button} onPress={handleLogin}>
+          <Text style={styles.buttonText}>Entrar</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.buttonSecondary} onPress={() => navigation.navigate('SignUpPage')}>
+          <Text style={styles.buttonTextSecondary}>Cadastrar</Text>
         </TouchableOpacity>
       </View>
-      <TouchableOpacity style={styles.button} onPress={handleLogin}>
-        <Text style={styles.buttonText}>Entrar</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('SignUpPage')}>
-        <Text style={styles.buttonText}>Cadastrar</Text>
-      </TouchableOpacity>
     </View>
   );
 }
@@ -63,70 +67,92 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     padding: 20,
-    backgroundColor: '#1E90FF',
+    backgroundColor: '#1E1E1E', // Fundo mais escuro e moderno
   },
   welcomeText: {
     fontSize: 28,
     fontWeight: 'bold',
     textAlign: 'center',
     marginBottom: 20,
-    color: '#fff',
+    color: '#fff', // Cor neon para um efeito tecnológico
+    textShadowColor: '#00FF00',
+    textShadowOffset: { width: 1, height: 1 },
+    textShadowRadius: 5,
   },
   image: {
-    width: '80%',
+    width: '70%',
     height: 250,
     alignSelf: 'center',
-    marginBottom: 30,
+    marginBottom: 20,
+    borderColor: '#00FF00',
+  },
+  card: {
+    backgroundColor: '#2E2E2E', // Card escuro
     borderRadius: 15,
-    borderWidth: 3, 
-    borderColor: '#fff', 
+    padding: 20,
+    marginHorizontal: 20,
+    shadowColor: '#00FF00', // Sombra neon
+    shadowOffset: { width: 0, height: 5 },
+    shadowOpacity: 0.3,
+    shadowRadius: 10,
+    elevation: 5,
   },
   inputContainer: {
     flexDirection: 'row',
     alignItems: 'center',
     borderWidth: 1,
-    borderColor: '#fff',
+    borderColor: '#00FF00', // Borda neon
     borderRadius: 10,
     marginBottom: 15,
-    backgroundColor: '#fff',
+    backgroundColor: '#3E3E3E', // Fundo escuro para os inputs
+    paddingHorizontal: 10,
     shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.3,
-    shadowRadius: 3.84,
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 5,
     elevation: 3,
   },
   icon: {
     paddingHorizontal: 10,
+    color: '#fff',
   },
   input: {
     height: 50,
     flex: 1,
     paddingHorizontal: 15,
+    color: '#FFF', // Texto branco para contraste
+    fontSize: 16,
   },
   button: {
-    backgroundColor: '#fff',
+    backgroundColor: '#00FF00', // Botão neon
     borderRadius: 10,
-    paddingVertical: 10,
-    paddingHorizontal: 12,
-    marginBottom: 10,
+    paddingVertical: 12,
+    marginVertical: 10,
     alignItems: 'center',
-    width: '70%',
+    width: '100%',
     alignSelf: 'center',
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
+    shadowColor: '#00FF00', // Sombra neon
+    shadowOffset: { width: 0, height: 5 },
     shadowOpacity: 0.3,
-    shadowRadius: 3.84,
-    elevation: 3,
+    shadowRadius: 10,
+    elevation: 5,
   },
   buttonText: {
     fontSize: 16,
     fontWeight: 'bold',
-    color: '#1E90FF',
+    color: '#1E1E1E', // Texto escuro para contraste
+  },
+  buttonSecondary: {
+    backgroundColor: 'transparent',
+    borderRadius: 10,
+    paddingVertical: 8,
+    alignItems: 'center',
+    width: '50%', // Reduziu o tamanho do botão de cadastrar
+    alignSelf: 'center',
+  },
+  buttonTextSecondary: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    color: '#00FF00',
   },
 });
